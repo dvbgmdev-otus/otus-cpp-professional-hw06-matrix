f8e6b2737b1cc18a189d856c9dc2d7c46124da6f
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-05-21 17:51:11 +0300

    feat(matrix): добавлен работающий пример

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
-#include "debug_log.h"
+#include "matrix.h"
+
+#include <iostream>
+#include <tuple>
 
 int main() {
-    DEBUG_LOG("Application started");
+    Matrix<int, 0> matrix;
+
+    for (MatrixIndex i = 0; i < 10; ++i) {
+        matrix[i][i] = static_cast<int>(i);
+        matrix[i][9 - i] = static_cast<int>(9 - i);
+    }
+
+    for (MatrixIndex row = 1; row <= 8; ++row) {
+        for (MatrixIndex column = 1; column <= 8; ++column) {
+            if (column > 1) {
+                std::cout << ' ';
+            }
+
+            std::cout << matrix[row][column];
+        }
+
+        std::cout << '\n';
+    }
+
+    std::cout << matrix.size() << '\n';
+
+    for (const auto cell : matrix) {
+        MatrixIndex row = 0;
+        MatrixIndex column = 0;
+        int value = 0;
+        std::tie(row, column, value) = cell;
+
+        std::cout << row << ' ' << column << ' ' << value << '\n';
+    }
 
     return 0;
 }
