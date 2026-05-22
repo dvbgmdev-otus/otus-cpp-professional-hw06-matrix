fe32d06301318ced8c6b1be39bc7ee25f5be0a57
Dmitry Bogomaz <d.v.bogomaz@gmail.com>
2026-05-22 10:42:12 +0300

    test(matrix): добавлен тест самоконтроля диагоналей

--- tests/unit/matrix_test.cpp
+++ tests/unit/matrix_test.cpp
@@ -14,9 +14,14 @@ protected:
     Matrix<int, -1> m_matrix;
 };
 
-#if (1)  // Пример из задания
+class MatrixSelfCheckTest : public ::testing::Test {
+protected:
+    Matrix<int, 0> m_matrix;
+};
 
-// 1. Чтение значения по умолчанию, запись ячейки и обход занятых ячеек.
+#if (1)  // 1. Пример из задания
+
+// 1.1 Чтение значения по умолчанию, запись ячейки и обход занятых ячеек.
 TEST_F(MatrixExampleTest, TaskExample_WhenCellAssigned_ThenOnlyAssignedCellIsOccupied) {
     ASSERT_EQ(m_matrix.size(), 0U);
 
@@ -44,4 +49,18 @@ TEST_F(MatrixExampleTest, TaskExample_WhenCellAssigned_ThenOnlyAssignedCellIsOcc
     EXPECT_EQ(count, 1U);
 }
 
-#endif  // Пример из задания
+#endif
+
+#if (1)  // 2.Самоконтроль
+
+// 2.1 Индексация оператором [] и количество занятых ячеек после заполнения диагоналей.
+TEST_F(MatrixSelfCheckTest, Diagonals_WhenFilledByBracketIndexing_ThenOccupiedCellCountIs18) {
+    for (MatrixIndex i = 0; i < 10; ++i) {
+        m_matrix[i][i] = static_cast<int>(i);
+        m_matrix[i][9 - i] = static_cast<int>(9 - i);
+    }
+
+    EXPECT_EQ(m_matrix.size(), 18U);
+}
+
+#endif  // Самоконтроль
